ead3bb8cde742e3136d2a69e4a89ff8a80e460d4
ayush129 <mittalayush129@gmail.com>
2021-06-22 16:26:07 +0530

    sscal ldd and sll optimized ;150 cycles reduced

--- xscal/sscal/main.c
+++ xscal/sscal/main.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
+/*
+float *sscal(int n , register float alpha ,register float *arr, int incx)
+{
+	register int i=0;
+	register int x=n-1;	
+	if (n%2!=0)
+	{
+		arr[x]*=alpha;
+		n=x;
+	}
+	
+	for (;i<n;i+=2)
+	{
+		arr[i]*=alpha;	//both these operations done
+		arr[i+1]*=alpha;//with the help of vector inst
+	}
 
+	return arr;
+
+}
+
+*/
 float *sscal(int n , float alpha , float *arr, int incx);
 float* main()
 {
-	int n=5,incx=1;	
-	float arr[5]={4,3,2,5,1};
+	int n=100,incx=1;
+	int i=0;	
+	float arr[100];
+	for (;i<n;i++)
+	arr[i]=4;
+	
+
 	float alpha=1.1;
 	return sscal(n,alpha,&arr[0],incx);
 }
--- xscal/sscal/sscal.s
+++ xscal/sscal/sscal.s
@@ -1,7 +1,8 @@
 !author : Ayush Mittal, Krithik sankar 
 !email  : mittalayush129@gmail.com, krithik.sankar10@gmail.com
 !date   : 10th June 2021
-
+!ldd instruction used instead of loading every word.
+!sll optimization
 .section ".bss"
 .common datak, 400,4
 
@@ -10,73 +11,43 @@
 .align 4
 .global sscal
 .type sscal, #function
+sscal:
+		save  %sp, -104, %sp
+		!st  %i0, [ %fp + 0x44 ]		!n
+		sll %i0, 2, %i0
 
-   sscal:	save  %sp, -104, %sp
-      		st  %i0, [ %fp + 0x44 ]
-      		st  %i1, [ %fp + 0x48 ]
-      		st  %i2, [ %fp + 0x4c ]
-      		st  %i3, [ %fp + 0x50 ]
+       	st  %i1, [ %fp + -4 ]
+       	ld  [ %fp + -4 ], %f8		!alphas in l8
+		
+       	mov  %i2, %g1				!arr
+       	clr  %i5					!i=0
+       	add  %i0, -4, %i4			!x=n-1
 
-      		ld  [ %fp + 0x48 ], %f10	!alpha
-      		ld  [ %fp + 0x48 ], %f11	!alpha
+       	and  %i0, 7, %g2			!n%2!=0
+       	cmp  %g2, 0
+       	be  for
+       	fmovs %f8, %f9			!l9 delay slot
+						
+       	add  %g1, %i4, %g2
 
-      		clr  [ %fp + -4 ]		!k=0
-      		clr  [ %fp + -8 ]		!i=0
-      		ld  [ %fp + 0x44 ], %g1		! n is even ?
-      		and  %g1, 1, %g1
-      		cmp  %g1, 0			! n is even ?
-      		bne  ac 			!b if odd  
-      		nop 
-      		ld  [ %fp + 0x44 ], %g1		!n is even
-      		st  %g1, [ %fp + -4 ]
-      		b  fc 				!b to for 
-      		nop 
-      ac:	ld  [ %fp + 0x44 ], %g1		!k=n-1
-      		add  %g1, -1, %g1		!k=n-1
-      		st  %g1, [ %fp + -4 ]		!k=n-1
-      		b  fc 				!b to for
-      		nop 
-      c0:	ld  [ %fp + -8 ], %g1		!i
-      		sll  %g1, 2, %g1		!i*4
-      		set  datak, %g2			!results stored in mem
-      		add  %g2, %g1, %g1		!mp=mp+(i*4) d
-      		ld  [ %fp + -8 ], %g2		!i
-      		sll  %g2, 2, %g2		!i*4
-      		ld  [ %fp + 0x4c ], %g3		!&arr[0]
-      		add  %g3, %g2, %g2		!arr=arr+(i*4) o
-      		ld  [ %g2 ], %f8		!load[arr+i*4] pair in f8 f9
-		ld  [ %g2 +4 ], %f9
-      		vfmul32 %f8, %f10, %f12		!multiply
-      		st  %f12, [ %g1 ]		!stroring in d
-		st  %f13, [ %g1 +4 ]		!stroring in d
-      		ld  [ %fp + -8 ], %g1		!i 
-      		add  %g1, 2, %g1		! i=i+2
-      		st  %g1, [ %fp + -8 ]		! update i 
-      fc:	ld  [ %fp + -8 ], %g2	!i   for loop 
-     		ld  [ %fp + -4 ], %g1	!k
-     		cmp  %g2, %g1		!if i< k
-     		bl  c0 			!go to body of for
-     		nop 
-     		ld  [ %fp + 0x44 ], %g1	! if n is odd
-     		and  %g1, 1, %g1	! if n is odd
-     		cmp  %g1, 0		! if n is odd 
-     		be  fd 	
-     		nop 
-     		ld  [ %fp + 0x44 ], %g1	! load n 
-     		add  %g1, -1, %g1	! n-1
-     		sll  %g1, 2, %g1	! (n-1)*4
-     		set datak, %g2 		! load mem addresss
-     		add  %g2, %g1, %g1	! last element addr d
-     		ld  [ %fp + 0x44 ], %g2 ! load n 
-     		add  %g2, -1, %g2	! n-1
-     		sll  %g2, 2, %g2	! (n-1)*4
-     		ld  [ %fp + 0x4c ], %g3	! arr
-     		add  %g3, %g2, %g2	! last element addr o
-     		ld  [ %g2 ], %f9
-     		fmuls  %f9, %f10, %f8	! last addition 
-     		st  %f8, [ %g1 ]	! storing the last element
-     fd:	set  datak, %g1 ! ld arr 
-     		mov  %g1, %i0		! returning the same array
-     		restore 
-     		retl 
-     		nop 
+       	ld  [ %g2 ], %f10
+       	fmuls  %f10, %f8, %f10
+       	st  %f10, [ %g2 ]
+	
+       	b  for
+       	mov %i4, %i0 			!delay slot filling
+
+body:  
+       	ldd  [ %g2 ], %f10
+		vfmul32 %f8, %f10, %f10
+       	std  %f10, [ %g2 ]
+       	add  %i5, 8, %i5
+for:	
+       	cmp  %i5, %i0
+       	bl,a  body
+       	add  %g1, %i5, %g2
+ 
+      	mov  %g1, %i0
+      	restore 
+      	retl 
+      	nop 
