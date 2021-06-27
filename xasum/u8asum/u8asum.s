2f1e681fe9b84c1b188b4123a7a9c3bd88ef2b53
ayush129 <mittalayush129@gmail.com>
2021-06-27 10:55:04 +0530

    u8asum abs optimized with and operation along with logical right shift logic

--- xasum/u8asum/u8asum.s
+++ xasum/u8asum/u8asum.s
@@ -9,9 +9,6 @@
 !a0 a1 a2......a7 in register a = byte
 !m0 m1 m2 .....m7
 
-.section ".bss"
-.common datak, 400,4
-
 .section ".text"
 .align 4
 .global u8asum
@@ -22,96 +19,49 @@ u8asum:
         !  %i0	!n
         !  %i1	!arr0
         !  %i3	!incx
-	    !set 0x40000000, %g1	!arr
+		set 0x80808080, %i4 ! mask for finding
+		set 0x80808080, %i5 ! absolute
 
-								!g3 contain arr in the beginning
-								!in while loop in contain the exact
-								!address
         cmp %i0,8
 	   	bl o
 	    mov %i1, %g3
 
-		ldsb  [ %g3 ], %l5		!mask formation in 		
-		slld %l5,8,%l5			!%l4 and %l6
+		ldd [%g3], %l0			! %l0 %l1 contain data
+		andd %l0, %i4, %o2		! %o2 %o3 %o4 %o5 used for
+		srld %o2, 1,   %o4		! mask
+		ord %o2, %o4,  %o2		!
+		srld %o2, 2,   %o4		!
+		ord %o2, %o4,  %o2		! to find the absolute
+		srld %o2, 4,   %o4		!
+		ord %o2, %o4,   %o2		!
+		xord %l0, %o2, %l0		!
+		vsubd8 %l0, %o2, %l0	
 
-		ldsb  [ %g3 + 1 ], %l5	!mask look like:
-		slld %l5,8,%l5			!0xff00ff00 if second 
-								!and fourth byte is negative
-		ldsb  [ %g3 + 2 ], %l5
-		slld %l5,8,%l5		
-
-		ldsb  [ %g3 + 3 ], %l5
-		slld %l5,8,%l5				 
-
-		ldsb  [ %g3 + 4 ], %l7			
-		slld %l7,8,%l7   
-
-		ldsb  [ %g3 + 5 ], %l7		
-		slld %l7,8,%l7
-
-		ldsb  [ %g3 + 6 ], %l7
-		slld %l7,8,%l7	
-
-		ldsb  [ %g3 + 7 ], %l7
-		slld %l7,8,%l7	
-
-		ldd [%g3], %l0			!%l4 contain mask for %l0	
-								!%l5 contain mask for %l1
-		mov %l6, %l5			
-		xord %l0, %l4, %l0
-		vsubd8 %l0, %l4, %l0	!l0 and l1 contain absolute value
-
-       
-		add  %i0, -8, %i0		!n-=8 remaining elements
-  
         b  while
-       	mov 8, %i4				!i=0 delay slot
+       	add  %i0, -8, %i0		!n-=8 remaining elements
 
 
-body:   ldsb  [ %g3 ], %l5		!mask formation in 		
-		slld %l5,8,%l5			!%l4 and %l6
+body:   ldd [%g3], %l2			! %l2 %l3 contain data
+		andd %l2, %i4, %o2		! %o2 %o3 %o4 %o5 used for
+		srld %o2, 1,   %o4		! mask
+		ord %o2, %o4,  %o2		!
+		srld %o2, 2,   %o4		!
+		ord %o2, %o4,  %o2		! to find the absolute
+		srld %o2, 4,   %o4		!
+		ord %o2, %o4,   %o2		!
+		xord %l2, %o2, %l2		!
+		vsubd8 %l2, %o2, %l2	!
 
-		ldsb  [ %g3 + 1 ], %l5	!mask look like:
-		slld %l5,8,%l5			!0xff00ff00 if second 
-								!and fourth byte is negative
-		ldsb  [ %g3 + 2 ], %l5
-		slld %l5,8,%l5		
-
-		ldsb  [ %g3 + 3 ], %l5
-		slld %l5,8,%l5				 
-
-		ldsb  [ %g3 + 4 ], %l7			
-		slld %l7,8,%l7   
-
-		ldsb  [ %g3 + 5 ], %l7		
-		slld %l7,8,%l7
-
-		ldsb  [ %g3 + 6 ], %l7
-		slld %l7,8,%l7	
-
-		ldsb  [ %g3 + 7 ], %l7
-		slld %l7,8,%l7	
-
-		ldd [%g3], %l2			!%l4 contain mask for %l2	
-								!%l5 contain mask for %l3
-		mov %l6, %l5			!%l4 and %l5 contain mask
-		xord %l2, %l4, %l2
-		vsubd8 %l2, %l4, %l2
-	  
 		vaddd8 %l0, %l2, %l0		
-
 		add  %i0, -8, %i0		!n-=8 remaining elements
-		add  %i4, 8, %i4		!i+=8 address index
-
 while:
      	cmp  %i0, 7
      	bg  body
-     	add %i1, %i4 ,%g3		!delay slot
+     	add %g3, 8, %g3
 		
 		mov -1, %g4
 		adddreduce8 %l0, %g4, %l0		
 
- 
 	  	cmp  %i0 ,0			! if no elements left
 	  	be def				! exit the function
 
@@ -175,8 +125,7 @@ sv:	    ldsb [ %g3 + 5 ], %l1
 	    add %l0, %l1, %l0
 
 	
-def:
-        mov  %l0, %i0			! exit
+def:	mov  %l0, %i0			! exit
         restore 
         retl 
         nop 
